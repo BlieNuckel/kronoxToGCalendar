144af50190934a8867d5846172b404ffa2372cf7
BlueNickel <work.lasse.face@gmail.com>
2021-09-02 02:37:42 +0200

    changed params again

--- gui/GUI.py
+++ gui/GUI.py
@@ -137,7 +137,7 @@ class Application(tk.Frame):
             self.root,
             variable=self.ical_option_var,
             value=OPTIONS["Select programme to get schedule"],
-            values=OPTIONS.keys(),
+            *OPTIONS["Select programme to get schedule"]
         )
         self.ical_options.grid(row=5, column=0)
         self.ical_option_var.trace_add(
